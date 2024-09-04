01c0d1144d8604aea1ebb1335e81baf1bbc730ce
Randy Stauner <randy@r4s6.net>
2024-09-04 16:39:12 -0700

    Add mappings to open diagnostic float window and loclist

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -3,3 +3,9 @@ vim.diagnostic.config({
     border = "rounded"
   }
 })
+
+-- Open floating window with diagnostics for given line.
+vim.keymap.set('n', '<Leader>df', vim.diagnostic.open_float)
+
+-- Populate location list with buffer diagnostics.
+vim.keymap.set('n', '<Leader>dl', vim.diagnostic.setloclist)
